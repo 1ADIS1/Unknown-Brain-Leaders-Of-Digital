5e0039f800bddc57bf51485029ae44c96e662fc7
root <root@vps-38005.vps-default-host.net>
2021-08-21 11:38:43 +0000

    processing

--- app/addons/csc_protocol_convert/controllers/frontend/protocol.php
+++ app/addons/csc_protocol_convert/controllers/frontend/protocol.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($mode == 'convert') {
-        $uploaded_data = fn_filter_uploaded_data('files', ['avi']);
+        $uploaded_data = fn_filter_uploaded_data('files', ['avi, mp4']);
         fn_print_die($uploaded_data, 'qwe');
     }
 }
---
+++ local_conf.php
@@ -0,0 +1,14 @@
+<?php
+
+$config['db_host'] = 'localhost';
+$config['db_name'] = 'rosatom';
+$config['db_user'] = 'root';
+$config['db_password'] = 'root';
+
+// Host and directory where software is installed on no-secure server
+$config['http_host'] = '185.233.37.254';
+$config['http_path'] = '/';
+
+// Host and directory where software is installed on secure server
+$config['https_host'] = '185.233.37.254';
+$config['https_path'] = '/';
